8f481aa036d6e355ac0c838e146dc4da7210ce45
Augustin Soulard <42175782+augustinsoulard@users.noreply.github.com>
2024-02-26 12:54:25 +0100

    v2.0.0

--- ENJEU_AuRA_AUGUSTIN_SOULARD/workflow/Creation_method_enjeu_AuRA.R
+++ ENJEU_AuRA_AUGUSTIN_SOULARD/workflow/Creation_method_enjeu_AuRA.R
@@ -61,7 +61,7 @@ BDC_STATUTS_17_FLORE = BDC_STATUTS_17_FLORE %>%
              "ZNIEFF.Déterminantes_Ain", 
              "ZNIEFF.Déterminantes_Allier", 
              "ZNIEFF.Déterminantes_Ardèche","ZNIEFF.Déterminantes_Cantal", 
-             "ZNIEFF.Déterminantes_Drôme", "ZNIEFF.Déterminantes_Haute.Saône", 
+             "ZNIEFF.Déterminantes_Drôme", "ZNIEFF.Déterminantes_Haute.Loire", 
             "ZNIEFF.Déterminantes_Haute.Savoie", 
              "ZNIEFF.Déterminantes_Isère", 
              "ZNIEFF.Déterminantes_Loire", 
@@ -92,38 +92,171 @@ for(i in 1:length(levels(as.factor(BDC_STATUTS_17_FLORE$LB_STATUT_COL)))){
 #### Ajout des plantes indicatrices ZH et des EEEs
 #Chargement flore ZH
 FloreZH = read.csv("../../../01_MINI_OUTILS/ZONE_HUMIDE/RfloreZH/FloreZH.csv")
+# FloreZH = read.csv("../../../ZONE_HUMIDE/RfloreZH/FloreZH.csv")
+TAXREFv17_FLORE_JOIN = left_join(TAXREFv17_FLORE_JOIN,FloreZH,by=c("CD_REF"="CODE.FVF"))
 
+#Chargement flore EEE
+EEE_AuRA <- read.csv("../../../BDD_FLORE_CONSTRUCT/EEE/AuRA/EEE_AuRA.csv",h=T)
 
+for(i in 1:nrow(EEE_AuRA)){
+  if(EEE_AuRA$LAVERGNE_AUVERGNE[i] == ""){
+    EEE_AuRA$PointAUVERGNE[i] = 0
+  } else if(EEE_AuRA$LAVERGNE_AUVERGNE[i] == "2+" | EEE_AuRA$LAVERGNE_AUVERGNE[i] == "2" | EEE_AuRA$LAVERGNE_AUVERGNE[i] == "2 et 2+"){
+    EEE_AuRA$PointAUVERGNE[i] = 2
+  } else if(EEE_AuRA$LAVERGNE_AUVERGNE[i] == "3" ){
+    EEE_AuRA$PointAUVERGNE[i] = 3
+  } else if(EEE_AuRA$LAVERGNE_AUVERGNE[i] == "4" ){
+    EEE_AuRA$PointAUVERGNE[i] = 4
+  }else if(EEE_AuRA$LAVERGNE_AUVERGNE[i] == "5" ){
+    EEE_AuRA$PointAUVERGNE[i] = 5
+  }else if(EEE_AuRA$LAVERGNE_AUVERGNE[i] == "A signaler" ){
+    EEE_AuRA$PointAUVERGNE[i] = 5
+  }
+  if(EEE_AuRA$LAVERGNE_RHONE_ALPES[i] == ""){
+    EEE_AuRA$PointRHONE_ALPES[i] = 0
+  } else if(EEE_AuRA$LAVERGNE_RHONE_ALPES[i] == "1"){
+    EEE_AuRA$PointRHONE_ALPES[i] = 1
+  } else if(EEE_AuRA$LAVERGNE_RHONE_ALPES[i] == "2+" | EEE_AuRA$LAVERGNE_RHONE_ALPES[i] == "2" | EEE_AuRA$LAVERGNE_RHONE_ALPES[i] == "2 et 2+"){
+    EEE_AuRA$PointRHONE_ALPES[i] = 2
+  } else if(EEE_AuRA$LAVERGNE_RHONE_ALPES[i] == "3" ){
+    EEE_AuRA$PointRHONE_ALPES[i] = 3
+  } else if(EEE_AuRA$LAVERGNE_RHONE_ALPES[i] == "4" ){
+    EEE_AuRA$PointRHONE_ALPES[i] = 4
+  }else if(EEE_AuRA$LAVERGNE_RHONE_ALPES[i] == "5" ){
+    EEE_AuRA$PointRHONE_ALPES[i] = 5
+  }else if(EEE_AuRA$LAVERGNE_RHONE_ALPES[i] == "27" ){
+    EEE_AuRA$PointRHONE_ALPES[i] = 4
+  }
+  
+  if(EEE_AuRA$PointAUVERGNE[i] >= EEE_AuRA$PointRHONE_ALPES[i]){
+    EEE_AuRA$LAVERGNE_AURA[i] = EEE_AuRA$LAVERGNE_AUVERGNE[i]
+  } else (EEE_AuRA$LAVERGNE_AURA[i] = EEE_AuRA$LAVERGNE_RHONE_ALPES[i])
+  
+}
+
+EEE_AuRA_JOIN = EEE_AuRA %>% 
+  filter(CD_NOM != "NOMATCH" & CD_NOM != "") %>%
+  select(CD_NOM,LAVERGNE_AURA)
+EEE_AuRA_JOIN$CD_NOM = as.integer(EEE_AuRA_JOIN$CD_NOM)
+
+TAXREFv17_FLORE_JOIN = left_join(TAXREFv17_FLORE_JOIN,EEE_AuRA_JOIN,by=c("CD_REF"="CD_NOM"))
 
 #Selection des colonnes pour le tableau final
-TAXREFv17_FLORE_JOIN %>% select(CD_NOM = CD_REF,TRIGRAMME,NOM_VALIDE,
+TAXREFv17_FLORE_JOIN = TAXREFv17_FLORE_JOIN %>% select(CD_NOM = CD_REF,
+                                TRIGRAMME,
+                                NOM_VALIDE,
                                 NOM_VERN,INDIGENAT = FR, 
-                                EVEE_FR = Lutte.contre.certaines.espèces_France.métropolitaine,
+                                EVEE_LAVERGNE = LAVERGNE_AURA,
                                 DH = Directive.Habitat_France.métropolitaine,
                                 PN = Protection.nationale_France.métropolitaine,
                                 LRN = Liste.rouge.nationale_France.métropolitaine,
-                                PR =,
-                                PR_Corr =,
-                                LRR = ,
-                                ZNIEFF = ,
-                                PD01 = ,
-                                PD03 = ,
-                                PD07 = ,
-                                PD15 =,
-                                PD26 =,
-                                PD38 = ,
-                                PD42 = ,
-                                PD43 = ,
-                                PD63 = ,
-                                PD69 = ,
-                                PD73 = ,
-                                PD74 = ,
-                                Barcelonne =,
-                                Berne = ,
-                                Mondiale = ,
-                                Europe =,
+                                PR = Protection.régionale_Rhône.Alpes,
+                                PR_Corr = Protection.régionale_Rhône.Alpes,
+                                LRR = Liste.rouge.régionale_Rhône.Alpes,
+                                ZNIEFF = ZNIEFF.Déterminantes_Ain,# Les ZNIEFF départementale sont toutes les mêmes
+                                #ZNIEFF03 = ZNIEFF.Déterminantes_Allier,
+                                #ZNIEFF07 = ZNIEFF.Déterminantes_Ardèche,
+                                #ZNIEFF15 = ZNIEFF.Déterminantes_Cantal,
+                                #ZNIEFF26 = ZNIEFF.Déterminantes_Drôme,
+                                #ZNIEFF38 = ZNIEFF.Déterminantes_Isère,
+                                #ZNIEFF42 = ZNIEFF.Déterminantes_Loire,
+                                #ZNIEFF43 = ZNIEFF.Déterminantes_Haute.Loire,
+                                #ZNIEFF63 = ZNIEFF.Déterminantes_Puy.de.Dôme,
+                                #ZNIEFF69 = ZNIEFF.Déterminantes_Rhône,
+                                #ZNIEFF73 = ZNIEFF.Déterminantes_Savoie,
+                                #ZNIEFF74 = ZNIEFF.Déterminantes_Haute.Savoie,
+                                PD01 = Protection.départementale_Ain,
+                                PD26 = Protection.départementale_Drôme,
+                                PD38 = Protection.départementale_Isère,
+                                PD42 = Protection.départementale_Loire,
+                                PD74 = Protection.départementale_Haute.Savoie,
+                                indicatrice_ZH = INDIC_ZH,
+                                Barcelonne =Convention.de.Barcelone_France.métropolitaine,
+                                Berne = Convention.de.Berne_France.métropolitaine,
+                                Mondiale = Liste.rouge.mondiale_Monde,
+                                Europe = Liste.rouge.européenne_Europe,
                                 )
 
+## Création de la colonne ZNIEFF AuRA
+# ZNIEFFGlobale = function(x){
+#   valeur = NULL
+#   if(!is.na(x["ZNIEFF01"]) && x["ZNIEFF01"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF01")
+#   }
+#   if(!is.na(x["ZNIEFF03"]) && x["ZNIEFF03"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF03")
+#   }
+#   if(!is.na(x["ZNIEFF07"]) && x["ZNIEFF07"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF07")
+#   }
+#   if(!is.na(x["ZNIEFF15"]) && x["ZNIEFF15"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF15")
+#   }
+#   if(!is.na(x["ZNIEFF26"]) && x["ZNIEFF26"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF26")
+#   }
+#   if(!is.na(x["ZNIEFF38"]) && x["ZNIEFF38"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF38")
+#   }
+#   if(!is.na(x["ZNIEFF42"]) && x["ZNIEFF42"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF42")
+#   }
+#   if(!is.na(x["ZNIEFF43"]) && x["ZNIEFF43"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF43")
+#   }
+#   if(!is.na(x["ZNIEFF63"]) && x["ZNIEFF63"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF63")
+#   }
+#   if(!is.na(x["ZNIEFF69"]) && x["ZNIEFF69"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF69")
+#   }
+#   if(!is.na(x["ZNIEFF73"]) && x["ZNIEFF73"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF73")
+#   }
+#   if(!is.na(x["ZNIEFF74"]) && x["ZNIEFF74"] != "-"){
+#     valeur = paste0(valeur," ","ZNIEFF74")
+#   }
+#   if(is.null(valeur)){
+#     valeur = "-"
+#   }
+#   return(valeur)
+# }
+
+# TAXREFv17_FLORE_JOIN$ZNIEFF = apply(TAXREFv17_FLORE_JOIN,1,ZNIEFFGlobale)
+# TAXREFv17_FLORE_JOIN[TAXREFv17_FLORE_JOIN$ZNIEFF != "-",]$ZNIEFF = "D" #Car ici c'ets une ZNIEFF régionale
+TAXREFv17_FLORE_JOIN[!is.na(TAXREFv17_FLORE_JOIN$ZNIEFF),]$ZNIEFF = "D"
+
+#Création de la colonne Protection AuRA
+ ProtectionAuRA = function(x){
+   valeur = NULL
+   if(!is.na(x["PN"]) && x["PN"] != "-"){
+     valeur = paste0(valeur," ","PN")
+   }
+   if(!is.na(x["PR"]) && x["PR"] != "-"){
+     valeur = paste0(valeur," ","PR")
+   }
+  if(!is.na(x["PD01"]) && x["PD01"] != "-"){
+    valeur = paste0(valeur," ","PD01")
+    }
+   if(!is.na(x["PD26"]) && x["PD26"] != "-"){
+     valeur = paste0(valeur," ","PD26")
+   }
+   if(!is.na(x["PD38"]) && x["PD38"] != "-"){
+     valeur = paste0(valeur," ","PD38")
+   }
+   if(!is.na(x["PD42"]) && x["PD42"] != "-"){
+    valeur = paste0(valeur," ","PD42")
+    }
+   if(!is.na(x["PD74"]) && x["PD74"] != "-"){
+     valeur = paste0(valeur," ","PD74")
+   }
+   if(is.null(valeur)){
+    valeur = "-"
+  }
+  return(valeur)
+}
+
+TAXREFv17_FLORE_JOIN$PROTECTION_AURA = apply(TAXREFv17_FLORE_JOIN,1,ProtectionAuRA)
 
 #Enregistrement du tableau a integrer à la methode enjeu PACA
 write.csv(TAXREFv17_FLORE_JOIN,file = "TAXREFv17_FLORE_JOIN.csv",row.names = F,fileEncoding = "UTF-8",na="-")
